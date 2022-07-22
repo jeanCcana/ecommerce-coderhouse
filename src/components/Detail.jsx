3c39c89b929288c5e7bdcbead3fee97479b261f3
jeanCcana <jpcy18@gmail.com>
2022-07-22 11:59:52 -0500

    Updates

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -15,11 +15,42 @@ function Detail() {
         <h1 className="text-lg font-semibold uppercase">Detalle</h1>
         <FontAwesomeIcon icon={faBagShopping} className="text-xl" />
       </div>
-      <div className="flex-1 px-6 py-4 overflow-y-auto">
-        <img
-          className="w-full mb-3 px-2 brightness-110	mix-blend-multiply"
-          src={sneaker}
-        />
+      <div className="flex-1 overflow-y-auto">
+        <div className="px-7 py-4 shadow-lg rounded-3xl">
+          <img
+            className="w-full mb-3 px-6 brightness-110	mix-blend-multiply"
+            src={sneaker}
+          />
+          <div className="flex">
+            <div className="flex-1 mr-4">
+              <span className="font-bold uppercase text-xs block">Jordan</span>
+              <span className="uppercase font-semibold text-2xl block">
+                Jordan 1 Retro High OG
+              </span>
+              <span className="block">S/.1000</span>
+            </div>
+            <div>
+              <span className="py-1 px-2 rounded-md bg-black text-white text-sm">
+                Nuevo
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="px-7 pt-8">
+          <span>Tamaño-Hombres</span>
+          <div className="flex gap-4 mt-4 mb-8 justify-center">
+            {[38, 39, 40, 41, 42, 43].map((size) => (
+              <span className="px-3 py-2  border rounded-md ">{size}</span>
+            ))}
+          </div>
+          <span className="mb-2 block font-semibold">Descripción</span>
+          <p className="font-light text-gray-600 text-sm">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, in sunt
+            facere dolorum similique nesciunt laborum atque veniam reiciendis
+            expedita asperiores libero tempora dolorem eius quasi deleniti,
+            molestiae nemo quos!
+          </p>
+        </div>
       </div>
     </div>
   );
